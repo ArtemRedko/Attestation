8cabdd5ae40379679f1a7ae3a0c1c9540f9e887b
артём редько <redko.artyom@mail.ru>
2023-12-25 20:24:22 +0300

    Добавили код и еще пункт в описании решения про функции

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+
+string[] mass = {"Hello", "123", "geekbrains", "2024", "BM", "c#"};
+bool Symbols(string a){
+    if(a.Length <= 3){
+        return true;                 // Функция, проверяющая подходит ли строка под условие задачи
+    }
+    else return false;
+}
+int Number(string[] mass){
+    int n = 0;
+    foreach(string a in mass){
+        if(Symbols(a) == true){          // Функция, определяющая количество элементов в массиве, подходящих условию задачи
+            n++;
+        }
+    }
+    return n;
+}
+string[] NewMass(string[] mass){
+    int n = Number(mass);
+    string[] newmass = new string[n];
+    int j = 0;
+    for(int i = 0; i < mass.Length; i++){                       // Функция создания нового массива
+        bool res = Symbols(mass[i]);
+        if(res == true){
+            newmass[j] = mass[i];
+            j++;
+        }
+    }
+    return newmass;
+}
+void PrintMass(string[] newmass){
+    Console.Write("[");
+    for(int i = 0; i < newmass.Length - 1; i++){                    // Функция вывода нового массива
+        Console.Write($"{newmass[i]}, ");
+    }
+    Console.Write($"{newmass[newmass.Length - 1]}]");
+}
+string[] newmass = NewMass(mass);
+PrintMass(newmass);
+
+
+
+
+
+
+
+
